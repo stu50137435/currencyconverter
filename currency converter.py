476a6cc576ce5e80644414b907276d8bc7499959
stu50137435 <50137435@stu.gbmc.ac.uk>
2026-09-07 12:42:12 +0100

    Update currency converter.py
    added the missing lables, capital letters and other errors.

--- currency converter.py
+++ currency converter.py
@@ -4,16 +4,16 @@ from tkinter import *
 window = Tk()
 window.geometry("200x300")
 
-LBL_pounds = Lable(window, Text="pounds")
+LBL_pounds = Label(window, text="pounds")
 LBL_pounds.pack()
 
 txt_pounds = Entry(window, width=15)
-txt_pounds.pack
+txt_pounds.pack()
 
 bnt_convert = Button(window, text="convert")
 bnt_convert.pack(pady = 10)
 
-LBL_euros = Entry(window, Text = "euros")
+LBL_euros = Label(window, text = "euros")
 LBL_euros.pack()
 
 txt_euros = Entry(window, width=15)
